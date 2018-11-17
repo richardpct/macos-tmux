7c46dbae95f30b5a6953774af545a9568318858c
richardpct <noreply>
2018-11-17 22:44:54 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # macos-tmux
 tmux package for macOS
+
+# Requirements
+MacOS Mojave
+
+Tested with go version 1.11.1
+
+# Build
+$ go get -u github.com/richardpct/macos-tmux
+
+# Usage
+$ cd yourgobindir
+
+$ ./macos-tmux -destdir=whereyouwant
+
+where destdir is the target directory installation
---
+++ tmux.go
@@ -0,0 +1,90 @@
+// tmux package
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/richardpct/pkgsrc"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+)
+
+var destdir = flag.String("destdir", "", "directory installation")
+var pkg pkgsrc.Pkg
+
+const (
+	name     = "tmux"
+	vers     = "2.7"
+	ext      = "tar.gz"
+	url      = "https://github.com/tmux/tmux/releases/download/" + vers
+	hashType = "sha256"
+	hash     = "9ded7d100313f6bc5a87404a4048b3745d61f2332f99ec1400a7c4ed9485d452"
+)
+
+func checkArgs() error {
+	if *destdir == "" {
+		return fmt.Errorf("Argument destdir is missing")
+	}
+	return nil
+}
+
+func configure() {
+	fmt.Println("Waiting while configuring ...")
+	cmd := exec.Command("./configure", "--prefix="+*destdir)
+	cmd.Env = append(os.Environ(),
+		"LDFLAGS="+"-L"+*destdir+"/lib",
+		"CPPFLAGS="+"-I"+*destdir+"/include",
+	)
+	if out, err := cmd.Output(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("%s\n", out)
+	}
+}
+
+func build() {
+	fmt.Println("Waiting while compiling ...")
+	cmd := exec.Command("make")
+	if out, err := cmd.Output(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("%s\n", out)
+	}
+}
+
+func install() {
+	fmt.Println("Waiting while installing ...")
+	cmd := exec.Command("make", "install")
+	if out, err := cmd.Output(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("%s\n", out)
+	}
+}
+
+func main() {
+	flag.Parse()
+	if err := checkArgs(); err != nil {
+		log.Fatal(err)
+	}
+
+	pkg.Init(name, vers, ext, url, hashType, hash)
+	pkg.CleanWorkdir()
+	if !pkg.CheckSum() {
+		pkg.DownloadPkg()
+	}
+	if !pkg.CheckSum() {
+		log.Fatal("Package is corrupted")
+	}
+
+	pkg.Unpack()
+	wdPkgName := path.Join(pkgsrc.Workdir, pkg.PkgName)
+	if err := os.Chdir(wdPkgName); err != nil {
+		log.Fatal(err)
+	}
+	configure()
+	build()
+	install()
+}
